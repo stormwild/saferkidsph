e48ae0c968ce0ee8b694787024123c0a898fcefd
Alexander R. Torrijos <artorrijos@gmail.com>
2020-05-20 19:55:19 +0800

    Bootstrap navwalker refactor

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,14 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="navbar nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'walker' => new \App\wp_bootstrap4_navwalker()]) !!}
-      @endif
-    </nav>
-  </div>
-  {{-- @include('partials.navbar') --}}
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container">
+      <a class="navbar-brand brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu($primarymenu) !!}
-    @endif
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu($primarymenu) !!}
+      @endif
+    </div>
   </nav>
 </header>
