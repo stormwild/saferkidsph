9dca1b2c19b713d394dc04175470b6ff926860e7
Alexander R. Torrijos <artorrijos@gmail.com>
2020-06-12 16:12:07 +0800

    Updates

--- docs/push.sh
+++ docs/push.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-git tag -d v1.0.0
-git push origin :refs/tags/v1.0.0
 git tag -a v1.0.0 -m 'SaferKidsPH v1.0.0'
 git push origin master --tags
---
+++ docs/scripts.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+git tag -d v1.0.0
+git push origin :refs/tags/v1.0.0
